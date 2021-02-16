a9093405096caa9e3da2a76791837d1200546296
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-16 13:04:12 -0600

    Se eliminó el archivo metodos ya que no se logró ejecutar el método

--- src/core/Kemet.java
+++ src/core/Kemet.java
@@ -2,13 +2,46 @@ package core;
 import java.io.*;
 
 public class Kemet {
+	
+	public static void pPrinter(int limiter) {
+		for(int i=0; i<limiter; i++) {
+			for(int j=limiter-i; j>1; j--) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<=i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) throws IOException {
-		Metodos m=new Metodos();
+		boolean reset=true;
 		
-		System.out.println("¿De qué tamaño desea la pirámide? ");
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int cycle=Integer.parseInt(br.readLine());
-				
-		m.pyramidPrinter(cycle);
+		while(reset){
+			System.out.println(
+					"Opciones disponibles: "
+					+"\t1. Imprimir pirámide"
+					+"\t2. Taquilla"
+					+"\t3. Salir"
+					);
+			System.out.print("¿Qué desea hacer? ");
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			
+			switch(br.readLine().toLowerCase().charAt(0)) {
+			case 'i':
+				System.out.print("¿De qué tamaño desea la pirámide? ");
+				BufferedReader pNum=new BufferedReader(new InputStreamReader(System.in));
+				pPrinter(Integer.parseInt(pNum.readLine()));
+				break;
+			case 't':
+				System.out.println();
+				break;
+			case 's':
+				reset=false;
+			}
+		}
 	}
 }
