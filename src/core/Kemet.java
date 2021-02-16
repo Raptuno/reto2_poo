85fb65ef8418dd47f6ebbe2cbc784800002811f1
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-16 14:04:50 -0600

    Modificación final, por ahora...

--- src/core/Kemet.java
+++ src/core/Kemet.java
@@ -15,7 +15,35 @@ public class Kemet {
 		}
 	}
 	
-	
+	public static void boFunction() throws IOException { //Función de Taquilla
+		int aToll=100;
+		int nbToll=aToll-30;
+		int iToll=aToll-50;
+		
+		System.out.println(
+				"Tipos de tarifa: "
+				+"\n\t1. Adulto: $"+aToll
+				+"\n\t2. Niño o adulto mayor normal: $"+nbToll
+				+"\n\t3. Adulto Mayor INAPAM: $"+iToll
+				);
+		System.out.print("Seleccione una tarifa: ");
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		switch(br.readLine().toLowerCase().charAt(0)) {
+		case 'a':
+			System.out.println("Precio: "+aToll);
+			break;
+		case 'n':
+			System.out.println("Precio: "+nbToll);
+			break;
+		case 'i':
+			System.out.println("Precio: "+iToll);
+			break;
+		case 'b':
+			System.out.println("Precio. $"+nbToll);
+			break;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		boolean reset=true;
@@ -23,9 +51,9 @@ public class Kemet {
 		while(reset){
 			System.out.println(
 					"Opciones disponibles: "
-					+"\t1. Imprimir pirámide"
-					+"\t2. Taquilla"
-					+"\t3. Salir"
+					+"\n\t1. Imprimir pirámide"
+					+"\n\t2. Taquilla"
+					+"\n\t3. Salir"
 					);
 			System.out.print("¿Qué desea hacer? ");
 			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -34,10 +62,30 @@ public class Kemet {
 			case 'i':
 				System.out.print("¿De qué tamaño desea la pirámide? ");
 				BufferedReader pNum=new BufferedReader(new InputStreamReader(System.in));
-				pPrinter(Integer.parseInt(pNum.readLine()));
+				int cycle=Integer.parseInt(pNum.readLine());
+				if(cycle>=3) {
+					pPrinter(cycle);
+				} else {
+					System.out.println("Se requieren 3 o más asteriscos");
+				}
 				break;
 			case 't':
-				System.out.println();
+				boolean boLoop=true;
+				
+				while(boLoop) {
+					boFunction();
+					System.out.print("¿Ingresar otro boleto? ");
+					BufferedReader boLoopC=new BufferedReader(new InputStreamReader(System.in));
+					
+					confirm: switch(boLoopC.readLine().toLowerCase().charAt(0)) {
+					case 's':
+						break confirm;
+					case 'n':
+						boLoop=false;
+						break confirm;
+					}
+				}
+				
 				break;
 			case 's':
 				reset=false;
