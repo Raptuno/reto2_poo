d06c99bb5d1e9fa43da0c7b6161cb9d1d840cdc6
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-16 11:02:50 -0600

    Creación

---
+++ src/core/Kemet.java
@@ -0,0 +1,12 @@
+package core;
+import java.io.*;
+
+public class Kemet {
+	
+	private int cycle;
+	private BufferedReader br;
+	
+	public static void main(String[] args)throws IOException {
+		
+	}
+}
---
+++ src/core/Metodos.java
@@ -0,0 +1,8 @@
+package core;
+import java.io.*;
+
+public class Metodos {
+	public int setCycle(int y) {
+		return y;
+	}
+}
