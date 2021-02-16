74d293c5b86d9e1072f3404a57b4cc46ca358c5d
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-16 11:51:34 -0600

    Cambios varios, otra vez

--- src/core/Kemet.java
+++ src/core/Kemet.java
@@ -2,13 +2,13 @@ package core;
 import java.io.*;
 
 public class Kemet {
-	
-	private static int cycle;
-	
 	public static void main(String[] args) throws IOException {
-		
 		Metodos m=new Metodos();
 		
+		System.out.println("¿De qué tamaño desea la pirámide? ");
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int cycle=Integer.parseInt(br.readLine());
+				
 		m.pyramidPrinter(cycle);
 	}
 }
--- src/core/Metodos.java
+++ src/core/Metodos.java
@@ -3,12 +3,6 @@ import java.io.*;
 
 public class Metodos {
 	//Inicia Listado de métodos de la pirámide
-	public int setCycle(int y) throws IOException {
-		System.out.print("¿De qué tamaño desea la pirámide? ");
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		y=Integer.parseInt(br.readLine());
-		return y;
-	}
 	public void pyramidPrinter(int y) {
 		for(int i=0; i>y; i++) {
 			for(int j=(y-i); j>1; j++) {
