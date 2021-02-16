231b9a5ca45368f93f3de72f165ddb01deb8d817
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-16 11:09:54 -0600

    Adiciones a los métodos

--- src/core/Metodos.java
+++ src/core/Metodos.java
@@ -1,8 +1,22 @@
+//Clase usada para administrar los métodos sin errores de referencia estática a objetos no estáticos
+/*
+ * Si bien Eclipse me permite crear un archivo module-info.java para administras los módulos,
+ * decidí omitirlo debido a unos problemas de importación en el repositorio fairy-game-v2
+ */
+
 package core;
 import java.io.*;
 
 public class Metodos {
-	public int setCycle(int y) {
+	public int setCycle(int y) throws IOException{ //Método para inicializar el contador de ciclo
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		y=Integer.parseInt(br.readLine());
 		return y;
+	}
+	
+	public void pyramidPrinter(int y) {
+		for(int i=0; i<y; i++) {
+			System.out.println("*");
+		}
 	}
 }
